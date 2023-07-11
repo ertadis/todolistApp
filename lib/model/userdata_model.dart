def8fd204a17e40e519a88ec827bd4995e5f26ad
Ertadis <ertadis@hotmail.com>
2023-07-12 00:49:03 +0300

    Create userdata_model.dart
    API'den gelen list users içerisindeki kullanıcı verilerini işlemek için data model.

---
+++ lib/model/userdata_model.dart
@@ -0,0 +1,38 @@
+import 'dart:convert';
+
+class UserData {
+  int? id;
+  String? email;
+  String? firstName;
+  String? lastName;
+  String? avatar;
+
+  UserData({
+    this.id,
+    this.email,
+    this.firstName,
+    this.lastName,
+    this.avatar,
+  });
+
+  factory UserData.fromRawJson(String str) =>
+      UserData.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory UserData.fromJson(Map<String, dynamic> json) => UserData(
+        id: json["id"],
+        email: json["email"],
+        firstName: json["first_name"],
+        lastName: json["last_name"],
+        avatar: json["avatar"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "id": id,
+        "email": email,
+        "first_name": firstName,
+        "last_name": lastName,
+        "avatar": avatar,
+      };
+}
